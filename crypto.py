a788f69c554f0986c0cfc20e6bf5ab994d6db216
Dmitry Sichkar <dmmeteo@gmail.com>
2018-06-14 01:31:55 +0300

    Black

--- crypto.py
+++ crypto.py
@@ -7,14 +7,14 @@ import rsa
 
 
 # first user get the pubkey form second user
-message1 = b'-100000000000000.000000000000000000000000000000000000'
+message1 = b"-100000000000000.000000000000000000000000000000000000"
 crypto = rsa.encrypt(message1, pubkey2)
 message = rsa.decrypt(crypto, privkey2)
-print('first user: %s' % message1)
+print("first user: %s" % message1)
 
 
 # second user get the pubkey form first user
-message2 = b'+100000000000000.0000'
+message2 = b"+1000000.00000000"
 crypto = rsa.encrypt(message2, pubkey1)
 message = rsa.decrypt(crypto, privkey1)
-print('second user: %s' % message2)
+print("second user: %s" % message2)
