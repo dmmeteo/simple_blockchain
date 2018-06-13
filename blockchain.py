0c067243d20b3dcd30a0bbe7a08c2d781dd34b9f
Dmitry Sichkar <dmmeteo@gmail.com>
2018-06-13 22:36:22 +0300

    add Block

--- blockchain.py
+++ blockchain.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python
 import hashlib as h
 
-print('hello blockchain!')
+
+class Block:
+
+    def __init__(self, index, timestamp, data, previous_hash):
+        self.index = index
+        self.timestamp = timestamp
+        self.data = data
+        self.previous_hash = previous_hash
+        self.hash = self.hash_block()
+
+    def hash_block(self):
+        string_data = ", ".join(
+            [
+                str(self.index),
+                str(self.timestamp),
+                str(self.data),
+                str(self.previous_hash),
+            ]
+        )
+
+        sha = h.sha256()
+        sha.update(string_data.encode('utf-8'))
+
+        return sha.hexdigest()
